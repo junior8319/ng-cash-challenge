b1d97d4d9a973c9af1941eaa8be7bc2a81804b28
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-19 16:29:09 -0300

    backend-development -routes: Sets index of routes.

---
+++ backend/src/routes/index.routes.ts
@@ -0,0 +1,9 @@
+import usersRouter from './users.routes';
+import accountsRouter from './accounts.routes';
+import transactionsRouter from './transactions.routes';
+
+export {
+  usersRouter,
+  accountsRouter,
+  transactionsRouter,
+};
