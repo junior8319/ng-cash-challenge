9cd3fb415947b8e24cff0ed0529a428df665564e
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-20 07:43:14 -0300

    backend-development - routes: Addition oftoken  validation to users GET, PUT and DELETE routes.

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import UsersController from "../controllers/Users.controller";
 import errorMiddleware from "../middlewares/error.middleware";
+import validateToken from "../middlewares/validateToken.middleware";
 
 const usersRouter = Router();
 
 usersRouter.get(
   '/users',
+  validateToken,
   UsersController.getUsers,
   errorMiddleware.handleErrors
 );
@@ -18,12 +20,14 @@ usersRouter.post(
 
 usersRouter.put(
   '/users/:id',
+  validateToken,
   UsersController.updateUser,
   errorMiddleware.handleErrors,
 );
 
 usersRouter.delete(
   '/users/:id',
+  validateToken,
   UsersController.deleteUser,
   errorMiddleware.handleErrors,
 );
