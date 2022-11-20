414261f167df7d903f71839d2520f6ed5bbfcc0f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-20 07:33:02 -0300

    backend-development -validations: Sets middleware to validate tokens.

---
+++ backend/src/middlewares/validateToken.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import jwtGenerator from '../helpers/jwtGenerator';
+
+const validateToken = async (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401)
+    .json({ message: 'Token não encontrado.' });
+
+  const isValidToken = await jwtGenerator.verify(authorization);
+
+  if (!isValidToken) return res.status(401)
+    .json({ message: 'Token inválido.' });
+  
+  next();
+};
+
+export default validateToken;
