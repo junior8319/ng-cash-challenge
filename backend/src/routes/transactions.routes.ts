0abd9cddfcfbe1c20f8ea9d8ccf5cfac9860a33c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-20 07:43:58 -0300

    backend-development - routes: Addition of token  validation to transactions GET, POST and DELETE routes.

--- backend/src/routes/transactions.routes.ts
+++ backend/src/routes/transactions.routes.ts
@@ -1,23 +1,27 @@
 import { Router } from "express";
 import TransactionsController from "../controllers/Transactions.controller";
 import errorMiddleware from "../middlewares/error.middleware";
+import validateToken from '../middlewares/validateToken.middleware';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get(
   '/transactions',
+  validateToken,
   TransactionsController.getTransactions,
   errorMiddleware.handleErrors,
 );
 
 transactionsRouter.post(
   '/transactions',
+  validateToken,
   TransactionsController.createTransaction,
   errorMiddleware.handleErrors,
 );
 
 transactionsRouter.delete(
   '/transactions/:id',
+  validateToken,
   TransactionsController.deleteTransaction,
   errorMiddleware.handleErrors,
 );
