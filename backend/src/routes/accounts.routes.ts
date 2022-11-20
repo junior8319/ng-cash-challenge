94816122454f8c8688eea9a4aecabd440b717c54
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-20 07:42:13 -0300

    backend-development - routes: Addition of login  validation to accounts GET route.

--- backend/src/routes/accounts.routes.ts
+++ backend/src/routes/accounts.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import AccountsController from "../controllers/Accounts.controller";
 import errorMiddleware from "../middlewares/error.middleware";
+import validateToken from "../middlewares/validateToken.middleware";
 
 const accountsRouter = Router();
 
 accountsRouter.get(
   '/accounts',
+  validateToken,
   AccountsController.getAccounts,
   errorMiddleware.handleErrors
 );
